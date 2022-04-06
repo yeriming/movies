40ae166d92a761452a5557e4dceb75a2f87fc7b0
yeriming <100740625+yeriming@users.noreply.github.com>
2022-04-06 09:48:28 +0900

    innit commit

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { TextField } from '../components/TextField';
 
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="m-4">
+        <div className="text-3xl font-bold mb-10">New</div>
+        
+        <div className="py-40 bg-gray-200 text-gray-800 text-left font-bold text-2xl">아이스크림 맛집 구경해보세요
+        </div>
+        <div className="mt-5 text-3xl font-bold mb-10">List</div>
+
+        <div className="py-20 bg-white border border-gray-100 text-2xl text-gray-800 font-bold">Card Title</div>
+  
+      </div>
+  );
 };
--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import { TextField } from '../components/TextField';
 
 export const SignupPage = () => {
-  return <div>SignupPage</div>;
+  return  (
+   <div className="m-4">
+      <div className="text-3xl font-bold mb-10">회원가입</div>
+      
+      <div>
+        <TextField label="아이디" placeholder="아이디를 입력해주세요." />
+        <TextField label="비밀번호" placeholder="비밀번호를 입력해주세요."
+        type="password"/>
+        <TextField label="비밀번호 확인" placeholder="비밀번호를 입력해주세요."
+        type="password"/>
+        <TextField label="이름" placeholder="이름을 입력해주세요." />
+      </div>
+
+      <div className="mt-10">
+       <div className="py-4 mt-2 border border-gray-800 text-gray-800 text-center">회원가입</div>
+      </div>
+
+    </div>
+  );
 };
